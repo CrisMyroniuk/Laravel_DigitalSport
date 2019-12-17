37312f379971a0b9bcac342ab7972ecd9a433c70
Cris Myroniuk <nahuelmyroniuk1999@gmail.com>
2019-12-16 21:02:47 -0300

    Productos guardar

--- public/js/agregarProductoCarrito.js
+++ public/js/agregarProductoCarrito.js
@@ -1,7 +1,17 @@
 var buttonAgregarCarrito=document.getElementById('carrito');
 
-buttonAgregarCarrito.onclick=function(){
-  
-  swal("Producto Agregado!", "Has agregado un producto al carrito", "success");
+buttonAgregarCarrito.onclick=function(event){
+  var cantidad = document.querySelector(".cantidad");
+
+
+
+  if(cantidad.value == 0){
+    event.preventDefault();
+    swal("Cantidad invalida!", "Seleccione una cantidad mayor a 0", "info");
+  }
+  else{
+    swal("Producto Agregado!", "Has agregado un producto al carrito", "success");
+  }
+
 
 }
--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -55,7 +55,7 @@
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
     <a class="dropdown-item" href="/perfil">Mi Perfil</a>
-    <a class="dropdown-item" href="/carrito">Mi Carrito <a href=""></a> </a>
+    <a class="dropdown-item" href="/carrito">Mi Carrito <a href=""></a> </a> 
     <li><a class="dropdown-item" href="{{ route('logout') }}"
        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
       Cerrar Sesion
